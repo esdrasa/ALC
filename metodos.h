b51550dfbd65d6021d09197b4e289747739472c1
Mateus <mateusfscosta@gmail.com>
2015-10-17 20:47:40 +0000

    Blioteca para realização de operações avançadas em uma matriz

---
+++ metodos.h
@@ -0,0 +1,20 @@
+#ifndef METODOS_H_INCLUDED
+#define METODOS_H_INCLUDED
+
+/**
+Calcula e retorna o resíduo da solução de um sistema linear.
+Um sistema linear Ax = b, onde A é a matriz e x a solução encontrada.
+O cálculo do resíduo será: r = b - Ax
+*/
+double* residuo(double** matriz, double* x, double* b, int n);
+
+/**
+Realiza a fatoração LU na matriz A de tamanho n.
+Os argumentos l e u são as matrizes L e U, respectivamente.
+l e u são iniciados antes da chamada à função.
+A função retornará 0 caso algum valor na diagonal principal de u seja 0,
+e retornará 1 não ocorra problemas.
+*/
+int lu(double** a, double** l, double** u, int n);
+
+#endif // OPERACOES_H_INCLUDED
