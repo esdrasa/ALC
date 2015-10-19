77b960bb120f9fc83884e8ddf8f6fd19b51d9587
Mateus <mateusfscosta@gmail.com>
2015-10-19 15:15:16 +0000

    Biblioteca para entrada de matrizes e vetores

---
+++ entrada.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operacoes.h"
+
+double** lerMatriz(int n)
+{
+    FILE *fMatriz;
+    fMatriz = fopen("matriz.txt", "r");
+
+    double **matriz;
+    int i, j;
+
+    matriz = criaMatriz(n);
+
+    if (fMatriz == NULL)
+    {
+        printf("Erro na leitura do arquivo\n");
+        exit(0);
+    }
+
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            fscanf(fMatriz, "%lf,", &matriz[i][j]);
+
+    fclose(fMatriz);
+
+    return matriz;
+}
+
+double* lerVetor(int n)
+{
+    FILE *fVetor;
+    fVetor = fopen("vetor.txt", "r");
+
+    double *vetor;
+    int i;
+
+    vetor = criaVetor(n);
+
+    if (fVetor == NULL)
+    {
+        printf("Erro na leitura do arquivo\n");
+        exit(0);
+    }
+
+    for(i = 0; i < n; i++)
+        fscanf(fVetor, "%lf,", &vetor[i]);
+
+    fclose(fVetor);
+
+    return vetor;
+}
