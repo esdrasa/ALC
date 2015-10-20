65e0d563d1a4afe8c164a584752ce46c2cbe98ca
Mateus <mateusfscosta@gmail.com>
2015-10-20 18:49:40 +0000

    Biblioteca para entrada de matrizes e vetores

--- entrada.h
+++ entrada.h
@@ -1,8 +1,14 @@
 #ifndef ENTRADA_H_INCLUDED
 #define ENTRADA_H_INCLUDED
 
+/**
+Função para ler uma matriz quadrada de tamnho n do arquivo matriz.txt
+*/
 double** lerMatriz(int n);
 
+/**
+Função para ler um vetor de tamnho n do arquivo vetor.txt
+*/
 double* lerVetor(int n);
 
 #endif // ENTRADA_H_INCLUDED
