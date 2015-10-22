51cfdc7ed5c3737d8c5e4f663a3ba3b9ab75e691
Mateus <mateusfscosta@gmail.com>
2015-10-22 03:32:28 +0000

    Adição de funções para resolução de sistemas com métodos iterativos e seus critérios.

--- metodos.c
+++ metodos.c
@@ -37,3 +37,201 @@ int lu(double** a, double** l, double** u, int n)
 
     return 1;
 }
+
+int jacobi(double** A, double* b, double* x, double tolerancia, unsigned long int iMax, int n)
+{
+    int i, j;
+    double erro = tolerancia + 1, *xant, soma;
+    unsigned long int k = 0;
+
+    xant = criaVetor(n);
+
+    for(i = 0; i < n; i++)
+    {
+        /*
+        Se algum elemento da diagonal principal for zero, não haverá solução,
+        a menos que as linha da matriz sejam permutadas.
+        */
+        if(A[i][i] == 0)
+            return 0;
+
+        //primeiro vetor para solução do método iterativo
+        x[i] = b[i] / A[i][i];
+    }
+
+    while(k < iMax && erro >= tolerancia)
+    {
+        if(erro == 0 && tolerancia == 0)
+            return 1;
+
+        erro = 0;
+
+        for(i = 0; i < n; i++)
+            xant[i] = x[i];
+
+        for(i = 0; i < n; i++)
+        {
+            soma = 0;
+
+            for(j = 0; j < n; j++)
+                if(j != i)
+                    soma += A[i][j] * xant[j];
+
+            x[i] = (b[i] - soma) / A[i][i];
+
+            if(erroRelativo(x[i], xant[i]) > erro)
+                erro = erroRelativo(x[i], xant[i]);
+        }
+
+        k++;
+    }
+
+    /*
+    Se erro for maior que a tolerancia no final das iterações, significa que
+    não ocorreu convergência para o número máximo de iterações definidas.
+    */
+    if(erro > tolerancia)
+        return 0;
+
+    return 1;
+}
+
+int gaussSeidel(double** A, double* b, double* x, double tolerancia, unsigned long int iMax, int n)
+{
+    int i, j;
+    double erro = tolerancia + 1, *xant, soma;
+    unsigned long int k = 0;
+
+    xant = criaVetor(n);
+
+    for(i = 0; i < n; i++)
+    {
+        /*
+        Se algum elemento da diagonal principal for zero, não haverá solução,
+        a menos que as linha da matriz sejam permutadas.
+        */
+        if(A[i][i] == 0)
+            return 0;
+
+        //primeiro vetor para solução do método iterativo
+        x[i] = b[i] / A[i][i];
+    }
+
+    while(k < iMax && erro >= tolerancia)
+    {
+        if(erro == 0 && tolerancia == 0)
+            return 1;
+
+        erro = 0;
+
+        for(i = 0; i < n; i++)
+            xant[i] = x[i];
+
+        for(i = 0; i < n; i++)
+        {
+            soma = 0;
+
+            for(j = 0; j < n; j++)
+                if(j != i)
+                    soma += A[i][j] * x[j];
+
+            x[i] = (b[i] - soma) / A[i][i];
+
+            if(erroRelativo(x[i], xant[i]) > erro)
+                erro = erroRelativo(x[i], xant[i]);
+        }
+
+        k++;
+    }
+
+    /*
+    Se erro for maior que a tolerancia no final das iterações, significa que
+    não ocorreu convergência para o número máximo de iterações definidas.
+    */
+    if(erro > tolerancia)
+        return 0;
+
+    return 1;
+}
+
+double criterioLinhas(double** A, int n)
+{
+    double soma;
+    int i, j;
+
+    for(i = 0; i < n; i++)
+    {
+        soma = 0;
+        for(j = 0; j < n; j++)
+            if(i != j)
+                soma += fabs(A[i][j]);
+
+        if(soma >= fabs(A[i][i]))
+            return 0;
+    }
+
+    return soma;
+}
+
+double criterioColunas(double** A, int n)
+{
+    double soma;
+    int i, j;
+
+    for(j = 0; j < n; j++)
+    {
+        soma = 0;
+        for(i = 0; i < n; i++)
+            if(i != j)
+                soma += (fabs(A[i][j]) / fabs(A[i][i]));
+
+        if(soma >= 1)
+            return 0;
+    }
+
+    return soma;
+}
+
+double criterioNorma(double** A, int n)
+{
+    double norma;
+
+    norma = normaF(A,n);
+
+    if(norma >= 1)
+        return 0;
+
+    return norma;
+}
+
+double criterioSassenfeld(double** A, int n)
+{
+    double *b, maior;
+    int i, j;
+
+    b = criaVetor(n);
+
+    for(i = 0; i < n; i++)
+    {
+        b[i] = 0;
+        for(j = 0; j < n; j++)
+        {
+            if(j < i)
+            {
+                b[i] += fabs(A[i][j]) * b[j];
+            }
+            else if(j > i)
+            {
+                b[i] += fabs(A[i][j]);
+            }
+        }
+        b[i] /= A[i][i];
+    }
+
+    maior = normaInfinito(b, n);
+
+    if(maior >= 1)
+        return 0;
+
+    return maior;
+}
