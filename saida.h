3fa804e16a6fd0001202773c6c1d02d1378e8ad6
Mateus <mateusfscosta@gmail.com>
2015-10-26 04:04:40 +0000

    Correção

--- saida.h
+++ saida.h
@@ -1,8 +1,14 @@
 #ifndef SAIDA_H_INCLUDED
 #define SAIDA_H_INCLUDED
 
-void imprimeMatriz(double** matriz, int n);
+/**
+Imprime uma matriz de ordem m x n no terminal
+*/
+void imprimeMatriz(double** matriz, int m, int n);
 
+/**
+Imprime um vetor de tamanho n no terminal
+*/
 void imprimeVetor(double* v, int n);
 
 #endif // SAIDA_H_INCLUDED
