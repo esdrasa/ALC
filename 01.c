bd342c374149492d277b46f2346b98afd53b7c22
Mateus <mateusfscosta@gmail.com>
2015-11-09 21:34:20 +0000

    Questão 01

--- 01.c
+++ 01.c
@@ -23,16 +23,20 @@ int main()
     printf("Norma de Frobenius: %lf\n", normaF(A, n));
     printf("Norma Linha: %lf\n", normaLinha(A, n));
     printf("Norma Coluna: %lf\n", normaColuna(A, n));
-
+    
+    //Atribui os elementos da coluna 1 (segunda coluna) ao vetor "coluna"
     getColuna(A, coluna, 1, n);
-
+    
+    //Produto interno entre a primeira linha e a segunda coluna
     produtoI = produtoEscalar(A[0], coluna, n);
-
+    
+    //Ângulo entre a primeira e linha e a segunda coluna 
     angle = angulo(A[0], coluna, n);
 
     printf("Produto interno entre a primeira linha e a segunda coluna: %lf\n", produtoI);
     printf("Angulo em radianos entre a primeira linha e a segunda coluna: %lf\n", angle);
-
+    
+    //Se a matriz lida do arquivo for uma matriz de Vandermonde, será possível calcular seu determinante
     if(teste)
         printf("Determinante: %lf\n", detVandermonde(A, n));
 
