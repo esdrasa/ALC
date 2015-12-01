d46a325be2b56172f7c64e6ba27b7baca400ccac
Angelo Victor <angelovst@hotmail.com>
2015-12-01 01:17:44 +0000

    Versao final, adicionado metodo de newton-raphson

--- 13.c
+++ 13.c
@@ -17,7 +17,7 @@ int main() {
 	printf("2) Metodo das Potencias\n");
 	printf("3) Metodo iterativo SOR\n");
 	printf("4) Fatoracao LU\n");
-	printf("5) PSO/Simulated annelling\n");
+	printf("5) Metodo de Newton-Raphson\n");
 	printf("6) Sair\n\n");
 
 	printf("Digite o numero do topico escolhido\n");
@@ -253,10 +253,66 @@ int main() {
 		getchar();
 	}//fecha o if da escolha 4
 
-	if(escolha == 5) {
-		if( (arquivo = fopen(".txt","r") ) == NULL )
+	if(escolha == 5)
+	{
+		system("clear");
+		if( (arquivo = fopen("newton.txt","r") ) == NULL )
 			printf("Nao foi possivel abrir o arquivo\n");
-	}
+
+		else
+		{
+				while(!feof(arquivo))
+				{	
+					fscanf(arquivo, "%c", &leitura);
+					printf("%c",leitura );					
+				}
+
+				double *polinomio, *raiz, tol, p0, p1;
+			    int n, i, r = 0;
+			    unsigned long int max;
+			    
+			    printf("Digite o grau do polinomio: ");
+			    scanf("%d%*c", &n);
+			    
+			    //Um polinômio de n graus possui n+1 coeficientes
+			    polinomio = criaVetor(n + 1);
+			    
+			    printf("Digite os coeficientes do polinomio, em ordem: ");
+			    for(i = 0; i <= n; i++)
+				scanf("%lf%*c", &polinomio[i]);
+			    
+			    printf("Digite uma tolerancia entre a raiz real e a raiz a ser calculada: ");
+			    scanf("%lf%*c", &tol);
+			    
+			    printf("Digite o numero maximo de iteracoes: ");
+			    scanf("%lu%*c", &max);
+			    
+			    raiz = criaVetor(1);
+			    
+			    if(newton(polinomio, raiz, tol, max, n))
+			    {
+				printf("Existe pelo menos uma raiz no polinomio digitado:\n");
+				printf("Raiz = %lf\n", *raiz);
+			    }
+			    else
+			    {
+				printf("Nao foram encontradas raizes neste polinomio.\n");
+			    }
+			    
+			    //Calcula p0 no ponto (0, p0) do polinômio dado.
+			    p0 = ordenada(polinomio, 0, n);
+			    
+			    //Calcula p1 no ponto (1, p1) do polinômio dado.
+			    p1 = ordenada(polinomio, 1, n);
+			    
+			    printf("Os seguintes pontos pertencem ao polinomio:\n(%d, %lf)\n(%d, %lf)\n", 0, p0, 1, p1);
+
+			    liberaVetor(polinomio);
+			    liberaVetor(raiz);
+		}
+		printf("Pressione enter para voltar ao Menu.\n");
+		getchar();
+	}//fecha o if da escolha 5
 
 	if(escolha == 6) {
 		return 0;
