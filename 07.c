ae5d68d67b30caced584963b50dbaf3cba5f7535
Angelo Victor <angelovst@hotmail.com>
2015-12-01 01:10:02 +0000

    Alteracoes para mostrar o sistema resolvido.

--- 07.c
+++ 07.c
@@ -5,20 +5,32 @@
 #include "entrada.h"
 
 int main()
-{
-    double **A, cLinha, cColuna, cNorma, cSassenfeld;
+{   
+    //declarando as variaveis a serem usadas
+    double **A, *b, *x, *xAux, cLinha, cColuna, cNorma, cSassenfeld, tol;
     int n;
+    unsigned long int iMax = 1000;
 
+    //lendo as informacoes necessarias
     printf("Digite o tamanho do sistema: ");
     scanf("%d%*c", &n);
 
+    //Alocando espaço para os vetores e matrizes
+    x = criaVetor(n);
+
+    xAux = criaVetor(n);
+
     A = lerMatriz(n, n);
 
+    b = lerVetor(n);
+
+    //executando os criterios
     cLinha = criterioLinhas(A, n);
     cColuna = criterioColunas(A, n);
     cNorma = criterioNorma(A, n);
     cSassenfeld = criterioSassenfeld(A, n);
 
+    //prints para verem se os criterios foram atendidos
     printf("\nCriterio das Linhas: ");
     if(cLinha)
         printf("[X]");
@@ -50,19 +62,39 @@ int main()
         printf("[ ]");
     printf(" %lf\n\n", cSassenfeld);
 
+    //Verificadno se os metodos podem ser usados
+    //a partir da garantia de convergencia
+    //dos criterios
+
     if(cNorma || cColuna || cLinha)
+    {
         printf("O sistema pode ser resolvido usando o metodo de Jacobi.\n");
+        jacobi(A, b, x, 0, iMax, n);
+        printf("A solucao do sistema pelo metodo de Jacobi e: \n");
+        imprimeVetor(x, n);
+    }
     
 
     if(cNorma || cColuna || cLinha || cSassenfeld)
-        printf("O sistema pode ser resolvido usando o metodo de Gauss-Seidel.\n");        
+    {
+        printf("O sistema pode ser resolvido usando o metodo de Gauss-Seidel.\n");
+        gaussSeidel(A, b, xAux, 0, iMax, n);
+        printf("A solucao do sistema pelo metodo de Gauss-Seidel e: \n");
+        imprimeVetor(xAux, n);
+    }
     
-
+    //Se os criterios nao garatem convergencia
+    //nada podemos afirmar
     if(!(cSassenfeld || cNorma || cColuna || cLinha))
         printf("Nada podemos afirmar sobre a resolucao do sistema pelos metodos.\n");
 
     getchar();
 
+    //liberando espaço alocado
+    liberaVetor(x);
+    liberaVetor(xAux);
+    liberaVetor(b);
     liberaMatriz(A, n);
+
     return 0;
 }
